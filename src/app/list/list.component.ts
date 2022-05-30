97bca98a792275f8ab523e6d11dc5779fb1f2dce
ThiagoFragaK <thiago@fragainformatica.com>
2022-05-30 20:31:01 -0300

    Added

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
+
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  brews: any[];
 
-  ngOnInit(): void {
+  constructor(private _http: HttpService) {
+    this.brews = [];
+   }
+  
+  ngOnInit() {
+    this._http.getBeer().subscribe(data => {
+      this.brews = data as any[]
+    }
+  );
   }
 
 }
